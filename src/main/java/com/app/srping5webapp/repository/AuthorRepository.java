045ef57479885a2f87f3387790489608554378ae
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-08-21 16:10:25 +0300

    add repositories

---
+++ src/main/java/com/app/srping5webapp/repository/AuthorRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016-2017 Sophos Limited. All rights reserved.
+ *
+ * 'Sophos' and 'Sophos Anti-Virus' are registered trademarks of Sophos Limited
+ * and Sophos Group.  All other product and company names mentioned are
+ * trademarks or registered trademarks of their respective owners.
+ */
+package com.app.srping5webapp.repository;
+
+import com.app.srping5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
