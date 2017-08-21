7d299737826bba98a0460eba73092e76ee06c0d4
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-08-21 22:29:23 +0300

    add data bootstrap

---
+++ src/main/java/com/app/srping5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016-2017 Sophos Limited. All rights reserved.
+ *
+ * 'Sophos' and 'Sophos Anti-Virus' are registered trademarks of Sophos Limited
+ * and Sophos Group.  All other product and company names mentioned are
+ * trademarks or registered trademarks of their respective owners.
+ */
+package com.app.srping5webapp.bootstrap;
+
+import com.app.srping5webapp.model.Author;
+import com.app.srping5webapp.model.Book;
+import com.app.srping5webapp.repository.AuthorRepository;
+import com.app.srping5webapp.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public DevBootstrap(final BookRepository bookRepository, final AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(final ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        //Rod
+        Author rod = new Author("Rod", "Jonson");
+        Book noEjb = new Book("JavaEE Development without EJB", "223344", "Worx");
+        rod.getBooks().add(noEjb);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEjb);
+    }
+}
